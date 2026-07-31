fe9d226516edd856579df8fdc5aebb495553517b
Elias Chaghouri <dhltracking@atomicmail.io>
2026-07-31 14:31:54 +1000

    udpated install.sh

--- install.sh
+++ install.sh
@@ -11,11 +11,13 @@ cp "$THEME_DIR/walker.css" \
 
 cp "$THEME_DIR/looknfeel.conf" ~/.config/omarchy/current/theme/looknfeel.conf
 cp "$THEME_DIR/colors.toml" ~/.config/omarchy/current/theme/colors.toml
+cp "$THEME_DIR/mako.ini" ~/.config/omarchy/current/theme/mako.ini
 
 mkdir -p ~/.config/omarchy/current/theme/backgrounds
 cp -r "$THEME_DIR/backgrounds/." ~/.config/omarchy/current/theme/backgrounds/
 
 omarchy-restart-waybar
 omarchy-restart-walker
+omarchy-restart-mako
 
 echo "✓ Liquid Glass installed."
